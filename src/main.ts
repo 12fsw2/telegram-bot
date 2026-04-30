dc77ed2bbfb34a35ddb678cab93d517d4ba0bc56
dushamboyevabror <abror2007d@gmail.com>
2026-04-30 10:11:36 +0500

    ulandi

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   const PORT = process.env.PORT || 3000;
+//   await app.listen(PORT, '0.0.0.0');
+//   console.log(`🤖 Matematik bot ishga tushdi! Port: ${PORT}`);
+// }
+
+// bootstrap();
+
+
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(bodyParser.json());
+
   const PORT = process.env.PORT || 3000;
+
   await app.listen(PORT, '0.0.0.0');
+
   console.log(`🤖 Matematik bot ishga tushdi! Port: ${PORT}`);
 }
 
