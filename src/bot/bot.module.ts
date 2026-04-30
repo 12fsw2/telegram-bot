3d3b1d3616af5fc8dd22a9ce3460decad749ef45
dushamboyevabror <abror2007d@gmail.com>
2026-04-30 10:26:21 +0500

    serverga ulandi

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,5 +1,26 @@
+// import { Module } from '@nestjs/common';
+// import { MongooseModule } from '@nestjs/mongoose';
+// import { BotService } from './bot.service';
+// import { BotUpdate } from './bot.update';
+// import { MathService } from '../math/math.service';
+// import { SessionService } from '../session/session.service';
+// import { User, UserSchema } from '../schemas/user.schema';
+
+// @Module({
+//   imports: [
+//     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+//   ],
+//   providers: [BotService, BotUpdate, MathService, SessionService],
+//   exports: [BotService],
+// })
+// export class BotModule {}
+
+
+
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TelegrafModule } from 'nestjs-telegraf';
+
 import { BotService } from './bot.service';
 import { BotUpdate } from './bot.update';
 import { MathService } from '../math/math.service';
@@ -9,6 +30,16 @@ import { User, UserSchema } from '../schemas/user.schema';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+
+    TelegrafModule.forRoot({
+      token: process.env.BOT_TOKEN as string,
+      launchOptions: {
+        webhook: {
+          domain: 'https://telegram-bot-gbvr.onrender.com',
+          hookPath: '/webhook',
+        },
+      },
+    }),
   ],
   providers: [BotService, BotUpdate, MathService, SessionService],
   exports: [BotService],
